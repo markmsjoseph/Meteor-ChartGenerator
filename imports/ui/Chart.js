6f7432a0d5a72bd4cff111895ba1e7a77e0950eb
Mark Joseph <markmsjoseph@gmail.com>
2018-07-06 00:25:53 -0400

    can select 1 axis or 2

--- imports/ui/Chart.js
+++ imports/ui/Chart.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {Bar, Line, Radar, Polar, Doughnut, Bubble, Scatter} from 'react-chartjs-2';
 import { createContainer } from 'meteor/react-meteor-data';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import ReactFileReader from 'react-file-reader';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 export  class BarChart extends React.Component {
 
@@ -10,23 +12,47 @@ export  class BarChart extends React.Component {
           this.state={
             entireFile:"",
             xAxisLabels:'',
-            firstColumnOfData:'',
-            secondCOlumnOfData:'',
-            twoPointPlot:'false',
-            chartType:'line'
+            singleDataSet:'',
+            twoPointDataSet:'',
+            chartType:'line',
+            tabIndex:0,
+            xyDataArray:''
 
           }
     }
 
+    //create array of x and y point objects
+    //called in render2PlainCharts
+    generateXandYdataPoints(xarray, yarray){
+
+        let xyArrayPoints = [];//data array which will hold objects of x and y values
+
+        //excel rows were not of x and y coordinate types
+        if(xarray.length != yarray.length){
+          console.log("Bad data input");
+        }
+        else{
+          for(var i=0; i<xarray.length; i++){
+            //create object of form {x:12,y:20}
+              const coordinatesObject = {x:xarray[i], y:yarray[i]}
+              console.log(coordinatesObject);
+              xyArrayPoints.push(coordinatesObject);
+          }
+          console.log(xyArrayPoints);
+        }
+        return xyArrayPoints;
+
+    }
+
     //we want to return an object that we can pass to the data prop on each chart
     //and where we can set the state of the labels and data itself
-    returnDataSet =() =>{
-      console.log("LABELS STATE", this.state.xAxisLabels);
+    returnDataSet =(data) =>{
+      console.log("in return dataaset");
       var dataObject ={
                              labels: this.state.xAxisLabels,
                              datasets: [{
                                            label: '# of Votes',
-                                           data: this.state.firstColumnOfData,
+                                           data: data,
                                            backgroundColor: [
                                               'rgba(255, 99, 132, 0.2)',
                                               'rgba(54, 162, 235, 0.2)',
@@ -47,17 +73,19 @@ export  class BarChart extends React.Component {
                              }]
                      }
 
-      console.log("DATAOBJECT", dataObject);
+
        return dataObject;
 
     }
 
-    //radio buttons switch between chart types
-    renderChartType(){
+
+    //displays the 1 axis chart
+    renderChartType(dataset){
+              console.log("renderChartType ", dataset);
               if(this.state.chartType == "line"){
                 return(
                   <Line
-                      data={this.returnDataSet()}
+                      data={dataset}
 
                      />
                 );
@@ -65,7 +93,7 @@ export  class BarChart extends React.Component {
               else if(this.state.chartType == "bar"){
                 return(
                   <Bar
-                      data={this.returnDataSet()}
+                      data={dataset}
 
                      />
                 );
@@ -73,7 +101,7 @@ export  class BarChart extends React.Component {
               else if(this.state.chartType == "polar"){
                 return(
                   <Polar
-                    data={this.returnDataSet()}
+                    data={dataset}
 
                      />
                 );
@@ -81,7 +109,7 @@ export  class BarChart extends React.Component {
               else if(this.state.chartType == "radar"){
                 return(
                   <Radar
-                      data={this.returnDataSet()}
+                      data={dataset}
 
                      />
                 );
@@ -89,13 +117,35 @@ export  class BarChart extends React.Component {
               else if(this.state.chartType == "doughnut"){
                 return(
                   <Doughnut
-                      data={this.returnDataSet()}
+                      data={dataset}
+
+                     />
+                );
+              }
+              else if(this.state.chartType == "scatter"){
+                return(
+                  <Scatter
+                      data={dataset}
+
+                     />
+                );
+              }
+              else if(this.state.chartType == "bubble"){
+                return(
+                  <Bubble
+                      data={dataset}
 
                      />
                 );
               }
     }
 
+    //sets state of xyDataArray to be an array of 2 poin objects
+    render2PlainCharts(){
+      let twoAxisData = this.generateXandYdataPoints(this.state.singleDataSet, this.state.twoPointDataSet);
+      console.log("2D",twoAxisData);
+      this.setState({xyDataArray: twoAxisData });
+    }
 
     //read in the csv file and process it in order to get the x axis labels and the dataset
     //the dataset will contain the first column with data, not multiple columns
@@ -111,125 +161,152 @@ export  class BarChart extends React.Component {
                   //set the labels and the data
                   this.setState({
                     xAxisLabels: arrayOfStrings[0].split(","),
-                    firstColumnOfData:arrayOfStrings[1].split(",")
+                    singleDataSet:arrayOfStrings[1].split(","),
+                    twoPointDataSet:arrayOfStrings[2].split(",")
                   });
-
+                  this.render2PlainCharts();
             }
           //contents of file will be converted to a string
           var entireFile = reader.readAsText(files[0]);
     }
 
+    //adds new label and data to the graphs with 1 axis
+    addNewDataPointAndLabel(e){
+        e.preventDefault();
+        //all refs are stored on the this.refs object, trim is to take off all leading and after spaces
+        let dataValue = this.refs.dataValue.value.trim();
+        let label = this.refs.label.value.trim();
 
-        //adds new label and data to the graphs
-        addNewDataPointAndLabel(e){
-            e.preventDefault();
-            //all refs are stored on the this.refs object, trim is to take off all leading and after spaces
-            let dataValue = this.refs.dataValue.value.trim();
-            let label = this.refs.label.value.trim();
-
-            this.setState((prevState) => ({
-              xAxisLabels: [...prevState.xAxisLabels, label],
-              firstColumnOfData: [...prevState.firstColumnOfData, dataValue]
-            }));
-
-
-        }
+        this.setState((prevState) => ({
+          xAxisLabels: [...prevState.xAxisLabels, label],
+          singleDataSet: [...prevState.singleDataSet, dataValue]
+        }));
 
 
-        //create array of x and y point objects
-        generateXandYdataPoints(xarray, yarray){
+    }
 
-            let xyArrayPoints = [];//data array which will hold objects of x and y values
+    //adds new x y coordinate and label
+    addNewXYDataPointAndLabel(e){
+      // e.preventDefault();
+      // //all refs are stored on the this.refs object, trim is to take off all leading and after spaces
+      // let xAxis = this.refs.xAxis.value.trim();
+      // let xAxis = this.refs.yAxis.value.trim();
+      // let label = this.refs.label.value.trim();
+      //
+      // const xyObject = {x:xAxis, y:yAxis}
+      // //update xy array of data containing xy points
+      // this.setState((prevState) => ({
+      //   xyDataArray: [...prevState.xyDataArray, xyObject],
+      //   xAxisLabels: [...prevState.xAxisLabels, label]
+      // }));
 
-            //excel rows were not of x and y coordinate types
-            if(xarray.length != yarray.length){
-              console.log("Bad data input");
-            }
-            else{
-              for(var i=0; i<xarray.length; i++){
-                //create object of form {x:12,y:20}
-                  const coordinatesObject = {x:xarray[i], y:yarray[i]}
-                  console.log(coordinatesObject);
-                  xyArrayPoints.push(coordinatesObject);
-              }
-              console.log(xyArrayPoints);
-            }
+    }
 
-        }
+    displayChartOptions(){
+      return(
+        <div>
+                  <label className="radioButtons">
+                        <input type="radio" name="button" value="line"  onChange={(e)=>{
+                        this.setState({chartType: e.target.value});
+                      }}/> Line
+                          <span className="checkmark"></span>
+                  </label>
 
-        // removeDataPoint(){
-        //       var array = this.state.people.filter(function(item) {
-        //         return item !== e.target.value
-        //       });
-        //
-        //
-        //
-        //       this.setState({
-        //         people: array
-        //       })
-        //
-        // }
+                  <label className="radioButtons">
+                        <input type="radio" name="button" value="bar" onChange={(e)=>{
+                          this.setState({chartType: e.target.value});
+                        }}/> Bar
+                          <span className="checkmark"></span>
+                  </label>
 
+                  <label className="radioButtons">
+                        <input type="radio" name="button" value="polar" onChange={(e)=>{
+                          this.setState({chartType: e.target.value});
+                        }}/> Polar
+                          <span className="checkmark"></span>
+                  </label>
+                  <label className="radioButtons">
+                        <input type="radio" name="button" value="radar" onChange={(e)=>{
+                          this.setState({chartType: e.target.value});
+                        }}/> Radar
+                          <span className="checkmark"></span>
+                  </label>
+
+                  <label className="radioButtons">
+                        <input type="radio" name="button" value="doughnut" onChange={(e)=>{
+                          this.setState({chartType: e.target.value});
+                        }}/> Doughnut
+                          <span className="checkmark"></span>
+                  </label>
+
+      </div>
+      );
+    }
+
+    displayChartOptions2d(){
+      return(
+        <div>
+
+                  <label className="radioButtons">
+                        <input type="radio" name="button" value="scatter" onChange={(e)=>{
+                          this.setState({chartType: e.target.value});
+                        }}/> Scatter
+                          <span className="checkmark"></span>
+                  </label>
+                  <label className="radioButtons">
+                        <input type="radio" name="button" value="bubble" onChange={(e)=>{
+                          this.setState({chartType: e.target.value});
+                        }}/> Bubble
+                          <span className="checkmark"></span>
+                  </label>
+      </div>
+      );
+    }
 
     render() {
-      this.generateXandYdataPoints([1,2,3,4,5], [6,7,8,9,10])
+      console.log("ARRAT WE WANT", this.state.xyDataArray);
               return (
                   <div className="container">
                             <div className="row justify-content-center selectCharts">
-
-                                        <label className="radioButtons">
-                                              <input type="radio" name="button" value="line"  onChange={(e)=>{
-                                              this.setState({chartType: e.target.value});
-                                            }}/> Line
-                                                <span className="checkmark"></span>
-                                        </label>
-
-                                        <label className="radioButtons">
-                                              <input type="radio" name="button" value="bar" onChange={(e)=>{
-                                                this.setState({chartType: e.target.value});
-                                              }}/> Bar
-                                                <span className="checkmark"></span>
-                                        </label>
-
-                                        <label className="radioButtons">
-                                              <input type="radio" name="button" value="polar" onChange={(e)=>{
-                                                this.setState({chartType: e.target.value});
-                                              }}/> Polar
-                                                <span className="checkmark"></span>
-                                        </label>
-                                        <label className="radioButtons">
-                                              <input type="radio" name="button" value="radar" onChange={(e)=>{
-                                                this.setState({chartType: e.target.value});
-                                              }}/> Radar
-                                                <span className="checkmark"></span>
-                                        </label>
-
-                                        <label className="radioButtons">
-                                              <input type="radio" name="button" value="doughnut" onChange={(e)=>{
-                                                this.setState({chartType: e.target.value});
-                                              }}/> Doughnut
-                                                <span className="checkmark"></span>
-                                        </label>
-                                        <br/>
-                                        <ReactFileReader handleFiles={this.handleFiles} fileTypes={'.csv'}>
-                                            <button className='btn'>Upload</button>
-                                        </ReactFileReader>
-
-                                        <form onSubmit={this.addNewDataPointAndLabel.bind(this)} >
-                                              <input  className = 'inputLoginFormStyles form-control form-control-lg' type="text" ref = "dataValue" placeholder= "Value of new datapoint"/>
-                                              <input className = 'inputLoginFormStyles form-control form-control-lg' type="text" ref = "label" placeholder= "X-axis label"/>
-                                               <button className=' button-login'> Add Data Point and label</button>
-                                        </form>
+                              { this.state.tabIndex== 0 ?  this.displayChartOptions() : this.displayChartOptions2d() }
+                              <br/>
+                              <ReactFileReader handleFiles={this.handleFiles} fileTypes={'.csv'}>
+                                  <button className='btn'>Upload</button>
+                              </ReactFileReader>
 
 
+                             <Tabs selectedIndex={this.state.tabIndex} onSelect={tabIndex => this.setState({ tabIndex })}>
+                                           <TabList className="tabtop">
+                                             <h1 className = "chatAppHeader"> Select Axis </h1>
+                                                      <CSSTransitionGroup transitionName="tabLoad" transitionAppear={true} transitionAppearTimeout={800}  transitionLeave={false} >
+                                                             <Tab>X Axis</Tab>
+                                                             <Tab>X & Y Axis</Tab>
+                                                     </CSSTransitionGroup>
+                                           </TabList>
 
-                                          {this.renderChartType()}
+                                           <TabPanel>
+                                             <form onSubmit={this.addNewDataPointAndLabel.bind(this)} >
+                                                   <input  className = 'inputLoginFormStyles form-control form-control-lg' type="text" ref = "dataValue" placeholder= "Value of new datapoint"/>
+                                                   <input className = 'inputLoginFormStyles form-control form-control-lg' type="text" ref = "label" placeholder= "X-axis label"/>
+                                                    <button className=' button-login'> Add Data Point on X-Axis and Label</button>
+                                             </form>
+                                           </TabPanel>
 
+                                             <TabPanel>
+                                               <form onSubmit={this.addNewDataPointAndLabel.bind(this)} >
+                                                     <input  className = 'inputLoginFormStyles form-control form-control-lg' type="text" ref = "xAxis" placeholder= "X-axis label"/>
+                                                     <input className = 'inputLoginFormStyles form-control form-control-lg' type="text" ref = "yAxis" placeholder= "Y-axis label"/>
+                                                     <input className = 'inputLoginFormStyles form-control form-control-lg' type="text" ref = "label" placeholder= "Label"/>
+                                                      <button className=' button-login'> Add X,Y Point</button>
+                                               </form>
+                                            </TabPanel>
+                              </Tabs>
+
+
+                                          { this.state.tabIndex== 0 ?  this.renderChartType(this.returnDataSet(this.state.singleDataSet)) : this.renderChartType(this.returnDataSet(this.state.xyDataArray)) }
 
                                 </div>
                   </div>
-
-
               );
     }
 
